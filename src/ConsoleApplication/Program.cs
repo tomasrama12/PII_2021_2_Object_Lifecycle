d8c42b641b8ea531263d21f2e2e82104744a35d3
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2021-09-01 03:36:55 -0300

    Program actualizado

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,9 +19,28 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            var train = new Train();
+            var train = new Train("algo");
             train.StartEngines();
             Console.WriteLine("Hello World!");
+
+            var t1 = new Train("Last Train To London");
+            var t2 = new Train("Last Train To London");
+            var t3 = new Train("Runaway Train");
+
+            // t1 y t2 NO son iguales ya que son variables que almacenan objetos diferentes,lo msimo con t2 y t3, en otras palabras
+            // cada variable apunta a un espacio de memoria diferente en el Heap.
+
+            
+
+            for (int i = 1; i < 10000000; i++)
+            {
+                Train TrenTest = new Train("chuchu");
+                // Al querer crearse 10.000.000 o grandes cantidades de objetos, el runtime se encarga de "destruir" aquellos que no son utilizados
+                // para liberar espacio en la memoria y así poder crear y almacenar otros nuevos.
+                
+            }
+            
+            Console.WriteLine(Train.Count);
         }
     }
 }
